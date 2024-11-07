e4cdeb816d1ade671d0d109e55fa81f98160d675
RUDRANSHFLY <mayamayavir@gmail.com>
2024-11-07 11:51:42 +0530

    Add CategorySelect to ProductsView for category filtering

--- src/components/products/ProductsView.tsx
+++ src/components/products/ProductsView.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { Category, Product } from "../../../sanity.types";
 import ProductsGrid from "./ProductsGrid";
+import CategorySelect from "../category/CategorySelect";
 
 interface ProductsViewProps {
   products: Product[] | [];
   categories: Category[] | [];
 }
 
-const ProductsView = ({ products }: ProductsViewProps) => {
+const ProductsView = ({ products, categories }: ProductsViewProps) => {
   return (
-    <div>
-      <ProductsGrid products={products} />
+    <div className={"flex flex-col"}>
+      <div className={"w-full sm:w-[200px]"}>
+        <CategorySelect category={categories} />
+      </div>
+
+      <div className={"flex-1"}>
+        <ProductsGrid products={products} />
+        <hr className={"w-1/2 sm:w-3/4"} />
+      </div>
     </div>
   );
 };
